d5ca96321a9322777807cee412bdf9bdc7e5ef67
Shugo Maeda <shugo@ruby-lang.org>
2017-04-20 21:31:44 +0900

    Show marks for unseen and flagged messages.

--- lib/mournmail/commands.rb
+++ lib/mournmail/commands.rb
@@ -13,12 +13,13 @@ module Mournmail
   end
 
   define_variable :current_mailbox
-  define_variable :current_mail_uid
+  define_variable :current_summary
+  define_variable :current_uid
   define_variable :current_mail
   define_variable :background_thread
 
   def self.background
-    if background_thread
+    if background_thread&.alive?
       raise EditorError, "Background thread already running"
     end
     self.background_thread = Utils.background {
@@ -65,6 +66,7 @@ module Mournmail
       @mailbox = mailbox
       @items = []
       @message_id_table = {}
+      @uid_table = {}
       @last_uid = nil
     end
 
@@ -78,7 +80,12 @@ module Mournmail
       if message_id
         @message_id_table[message_id] = item
       end
+      @uid_table[item.uid] = item
       @last_uid = item.uid
+    end
+
+    def [](uid)
+      @uid_table[uid]
     end
 
     def save
@@ -92,14 +99,15 @@ module Mournmail
   end
   
   class SummaryItem
-    attr_reader :uid, :date, :from, :subject
+    attr_reader :uid, :date, :from, :subject, :flags
     attr_reader :replies
     
-    def initialize(uid, date, from, subject)
+    def initialize(uid, date, from, subject, flags)
       @uid = uid
       @date = date
       @from = from
       @subject = subject
+      @flags = flags
       @line = nil
       @replies = []
     end
@@ -118,14 +126,19 @@ module Mournmail
       end
       s
     end
+
+    def set_flag(flag)
+      @flags.push(flag)
+      @line = nil
+    end
     
     private
 
     def format_line(limit = 78, from_limit = 16, level = 0)
       space = "  " * (level < 8 ? level : 8)
       s = String.new
-      s << format("%s  %s %s[ %s ] ",
-                  @uid, format_date(@date), space,
+      s << format("%s %s%s %s[ %s ] ",
+                  @uid, format_flags(@flags), format_date(@date), space,
                   ljust(format_from(@from), from_limit))
       s << ljust(decode_eword(@subject.to_s), limit - Buffer.display_width(s))
       s << "\n"
@@ -146,6 +159,16 @@ module Mournmail
         break if width == n
       end
       str + " " * (n - width)
+    end
+
+    def format_flags(flags)
+      if flags.include?(:Flagged)
+        "$"
+      elsif !flags.include?(:Seen)
+        "u"
+      else
+        " "
+      end
     end
     
     def format_date(date)
@@ -197,11 +220,13 @@ def mournmail_fetch_summary(mailbox)
     summary = Mournmail::Summary.load_or_new(mailbox)
     first_uid = (summary.last_uid || 0) + 1
     if first_uid != imap.responses["UIDNEXT"]&.last
-      data = imap.uid_fetch(first_uid..-1, ["UID", "ENVELOPE"])
+      data = imap.uid_fetch(first_uid..-1, ["UID", "ENVELOPE", "FLAGS"])
       data.each do |i|
         uid = i.attr["UID"]
         env = i.attr["ENVELOPE"]
-        item = Mournmail::SummaryItem.new(uid, env.date, env.from, env.subject)
+        flags = i.attr["FLAGS"]
+        item = Mournmail::SummaryItem.new(uid, env.date, env.from, env.subject,
+                                          flags)
         summary.add_item(item, env.message_id, env.in_reply_to)
       end
     end
@@ -229,8 +254,9 @@ define_command(:mournmail_visit_mailbox, doc: "Start mournmail.") do
       end
       switch_to_buffer(buffer)
       Mournmail.current_mailbox = mailbox
+      Mournmail.current_summary = summary
       Mournmail.current_mail = nil
-      Mournmail.current_mail_uid = nil
+      Mournmail.current_uid = nil
       message("Visited #{mailbox}")
     end
   end
@@ -244,6 +270,10 @@ define_command(:mournmail_quit, doc: "Quit mournmail.") do
   if buffer = Buffer["*message*"]
     kill_buffer(buffer)
   end
+  Mournmail.current_mailbox = nil
+  Mournmail.current_summary = nil
+  Mournmail.current_mail = nil
+  Mournmail.current_uid = nil
 end
 
 def mournmail_read_mail(mailbox, uid)
@@ -273,15 +303,14 @@ def mournmail_read_mail(mailbox, uid)
 end
 
 define_command(:mournmail_summary_read, doc: "Read a mail.") do
-  buffer = Buffer.current
+  summary_buffer = Buffer.current
   begin
-    uid = buffer.save_excursion {
-      buffer.beginning_of_line
-      return if !buffer.looking_at?(/\d+/)
+    uid = summary_buffer.save_excursion {
+      summary_buffer.beginning_of_line
+      return if !summary_buffer.looking_at?(/\d+/)
       match_string(0).to_i
     }
-    mailbox = Mournmail.current_mailbox
-    if uid == Mournmail.current_mail_uid
+    if uid == Mournmail.current_uid
       window = Mournmail.message_window
       if window.buffer.name == "*message*"
         old_window = Window.current
@@ -295,14 +324,15 @@ define_command(:mournmail_summary_read, doc: "Read a mail.") do
       end
     end
   rescue RangeError # may be raised by scroll_up
-    buffer.end_of_line
-    if buffer.end_of_buffer?
+    summary_buffer.end_of_line
+    if summary_buffer.end_of_buffer?
       raise EditorError, "No more mail"
     end
-    buffer.forward_line
+    summary_buffer.forward_line
     retry
   end
   Mournmail.background do
+    mailbox = Mournmail.current_mailbox
     mail = Mail.new(mournmail_read_mail(mailbox, uid))
     body = if mail.multipart?
       mail.text_part&.decoded
@@ -329,7 +359,20 @@ define_command(:mournmail_summary_read, doc: "Read a mail.") do
       end
       window = Mournmail.message_window
       window.buffer = message_buffer
-      Mournmail.current_mail_uid = uid
+      summary_item = Mournmail.current_summary[uid]
+      if summary_item && !summary_item.flags.include?(:Seen)
+        summary_item.set_flag(:Seen)
+        Mournmail.current_summary.save
+        summary_buffer.read_only_edit do
+          summary_buffer.save_excursion do
+            summary_buffer.beginning_of_line
+            if summary_buffer.looking_at?(/^(\d+) u/)
+              summary_buffer.replace_match('\1  ')
+            end
+          end
+        end
+      end
+      Mournmail.current_uid = uid
       Mournmail.current_mail = mail
     end
   end
--- lib/mournmail/summary_mode.rb
+++ lib/mournmail/summary_mode.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+Face.define :unseen, bold: true
+Face.define :flagged, foreground: "yellow", bold: true
+
 module Mournmail
   class SummaryMode < Textbringer::Mode
     SUMMARY_MODE_MAP = Keymap.new
     SUMMARY_MODE_MAP.define_key(" ", :mournmail_summary_read)
     SUMMARY_MODE_MAP.define_key("q", :mournmail_quit)
+
+    define_syntax :unseen, /^\d+ u.*/
+    define_syntax :flagged, /^\d+ \$.*/
 
     def initialize(buffer)
       super(buffer)
