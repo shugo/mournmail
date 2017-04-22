881228699081c6dc0925b7dc48578a4bf85af616
Shugo Maeda <shugo@ruby-lang.org>
2017-04-22 18:19:22 +0900

    Bind A to mournmail_summary_reply.

--- lib/mournmail/summary_mode.rb
+++ lib/mournmail/summary_mode.rb
@@ -12,6 +12,7 @@ module Mournmail
     SUMMARY_MODE_MAP.define_key("\C-h", :mournmail_summary_scroll_down)
     SUMMARY_MODE_MAP.define_key("\C-?", :mournmail_summary_scroll_down)
     SUMMARY_MODE_MAP.define_key("a", :mournmail_summary_reply)
+    SUMMARY_MODE_MAP.define_key("A", :mournmail_summary_reply)
     SUMMARY_MODE_MAP.define_key("q", :mournmail_quit)
     SUMMARY_MODE_MAP.define_key("k", :previous_line)
     SUMMARY_MODE_MAP.define_key("j", :next_line)
