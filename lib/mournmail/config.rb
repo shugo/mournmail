7b7cda5e39515d15055ede845fb2909868e8eb8f
Shugo Maeda <shugo@ruby-lang.org>
2019-10-01 06:46:34 +0900

    Change default commands for Windows and macOS

--- lib/mournmail/config.rb
+++ lib/mournmail/config.rb
@@ -18,7 +18,16 @@ module Textbringer
   ]
   CONFIG[:mournmail_imap_connect_timeout] = 10
   CONFIG[:mournmail_keep_alive_interval] = 60
-  CONFIG[:mournmail_file_open_comamnd] = "xdg-open"
-  CONFIG[:mournmail_link_open_comamnd] = "xdg-open"
+  case RUBY_PLATFORM
+  when /mswin|mingw/
+    CONFIG[:mournmail_file_open_comamnd] = "start"
+    CONFIG[:mournmail_link_open_comamnd] = "start"
+  when /darwin/
+    CONFIG[:mournmail_file_open_comamnd] = "open"
+    CONFIG[:mournmail_link_open_comamnd] = "open"
+  else
+    CONFIG[:mournmail_file_open_comamnd] = "xdg-open"
+    CONFIG[:mournmail_link_open_comamnd] = "xdg-open"
+  end
   CONFIG[:mournmail_addresses_path] = File.expand_path("~/.addresses")
 end
